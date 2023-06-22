f1588f9d07ff1f79da3e70734cdcccd1f1a32ae0
aliyounis0 <101262216+aliyounis0@users.noreply.github.com>
2023-06-22 16:39:36 +0300

    profile final "الحمدلله"

--- lib/presentation/pages/profile_view.dart
+++ lib/presentation/pages/profile_view.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: must_be_immutable
 
-import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -40,74 +39,74 @@ class ProfileView extends StatelessWidget {
       },
 
      builder: (context, state) {
-      return ConditionalBuilder(
-        condition: state is ProfileLoadingState,
-        builder:(context) =>const Center(child:  CircularProgressIndicator()),
-        fallback: (context)=>Scaffold(
-          appBar: AppBar(
-              backgroundColor: kPrimaryColor,
-              elevation: 0),
-          body:  Container(
-            margin: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                ListTile(
-                  title:const Text("Hesham Mohsen",
-                      style:TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20)),
-                  leading:  CircleAvatar(
-                      maxRadius: 25,
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Colors.red.withOpacity(0.5),
-                      backgroundImage: const AssetImage(AssetsData.avatar)
-                  ) ,
-                  trailing:  TextButton(onPressed: ()async{
-                    await appPreferences.setIsUserLoggedIn(false);
-                    await appPreferences.getOut();
-                    Get.off(()=>  LogInView());
-                    //Navigator.of(context).pushReplacementNamed(Routes.login);
-                  },
-                    child: const Text("Log Out",
-                        style: TextStyle(
-                            color: Colors.blueAccent,
-                            fontWeight: FontWeight.bold
-                        )),
+
+        if(state is ProfileSuccessState){
+          return Scaffold(
+            appBar: AppBar(
+                backgroundColor: kPrimaryColor,
+                elevation: 0),
+            body:  Container(
+              margin: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(state.profileData.userProfileData.name,
+                        style:const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20)),
+                    leading:  CircleAvatar(
+                        maxRadius: 25,
+                        backgroundColor: Colors.transparent,
+                        foregroundColor: Colors.red.withOpacity(0.5),
+                        backgroundImage: const AssetImage(AssetsData.avatar)
+                    ) ,
+                    trailing:  TextButton(onPressed: ()async{
+                      await appPreferences.setIsUserLoggedIn(false);
+                      await appPreferences.getOut();
+                      Get.off(()=>  LogInView());
+                      //Navigator.of(context).pushReplacementNamed(Routes.login);
+                    },
+                      child: const Text("Log Out",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontWeight: FontWeight.bold
+                          )),
+                    ),
                   ),
-                ),
-                SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03),
-                const UserInfo(
-                  label: "Email",
-                  text: "HeshamMohsen@gmail.com",
-                ),
-                const Divider(thickness: 1,),
-                const UserInfo(
-                  label: "Phone",
-                  text: "0101234567",
-                ),
-                const Divider(thickness: 1,),
-                const UserInfo(
-                  label: "National ID",
-                  text: "30001234567891",
-                ),
-                const Divider(thickness: 1,),
-                const UserInfo(
-                  label: "Gender",
-                  text: "male",
-                ),
-                const Divider(thickness: 1,),
-                const UserInfo(
-                  label: "Date of birth",
-                  text: "15/10/1985",
-                ),
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03),
+                   UserInfo(
+                    label: "Email",
+                    text: state.profileData.userProfileData.email,
+                  ),
+                  const Divider(thickness: 1,),
+                   UserInfo(
+                    label: "Phone",
+                    text: state.profileData.userProfileData.phone,
+                  ),
+                  const Divider(thickness: 1,),
+                   UserInfo(
+                    label: "National ID",
+                    text: state.profileData.userProfileData.id,
+                  ),
+                  const Divider(thickness: 1,),
+                   UserInfo(
+                    label: "Gender",
+                    text: state.profileData.userProfileData.gender,
+                  ),
+                  const Divider(thickness: 1,),
+                   UserInfo(
+                    label: "Date of birth",
+                    text: state.profileData.userProfileData.birthdate,
+                  ),
 
-                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                const ProfileCards(),
-              ],
-            ),),),
-      );
-
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                  const ProfileCards(),
+                ],
+              ),),);
+        }else{
+          return const Center(child: CircularProgressIndicator());
+        }
 
       },
 
